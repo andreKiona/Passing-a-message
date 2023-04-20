c99e6b5b7660d8d4bf5e145b226282e7529a118a
Andre Kiona <124687319+andreKiona@users.noreply.github.com>
2023-04-20 14:56:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
     form.addEventListener('submit', function(e){
         e.preventDefault();
 
+        /*Creating variables*/
         const message = document.querySelector('#message')
         const feedback = document.querySelector('.feedback')
         const messageContent = document.querySelector('.message-content')
@@ -12,11 +13,12 @@
             feedback.classList.add('show')
             setTimeout(function(){
                 feedback.classList.remove('show')
-            }, 4000)
+            }, 4000)/*Seetting up the clear time function*/
         } else{
             messageContent.textContent = message.value
             message.value = ''
         }
     })
 
+  /*creating a continuous loop with the below closing parethis*/
 })()
